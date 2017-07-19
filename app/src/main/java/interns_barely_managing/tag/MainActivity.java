493041e26bbe523adca2a5144a097df2794b0d9a
Noah Eigenfeld <noah.eigenfeld@ibm.com>
2017-07-19 13:26:32 -0500

    Project init

---
+++ app/src/main/java/interns_barely_managing/tag/MainActivity.java
@@ -0,0 +1,13 @@
+package interns_barely_managing.tag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
